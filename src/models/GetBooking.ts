a162a65a9558cfb7993ec30f133a01d3b56efa05
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2022-04-06 15:18:51 +0200

    Added check for picked day

---
+++ src/models/GetBooking.ts
@@ -0,0 +1,10 @@
+export class GetBooking {
+
+    constructor(
+        public _id: string, 
+        public restaurantId: string, 
+        public date: string, 
+        public time: string,
+        public numberOfGuests: number,
+        public customerId: string) {}
+};
